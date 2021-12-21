94139dd2937f273eec50edcaed58f2b27975254a
jsotobor <jsotobor@SVQ-1WCF9C3.usersad.everis.int>
2021-12-21 18:02:24 +0100

    apicircuit

---
+++ src/test/java/com/example/apicircuitbreaker/ApicircuitbreakerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.apicircuitbreaker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApicircuitbreakerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
